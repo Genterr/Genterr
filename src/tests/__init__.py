8c41b3330b09f03033839aae0214de03b93f3aba
Genterr <196460853+Genterr@users.noreply.github.com>
2025-02-07 21:53:36 +0100

    Add basic test structure

--- src/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-# src/tests/__init__.py
-# Created: 2025-02-01 21:23:38
-# Author: Genterr
-
-import sys
-import os
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
